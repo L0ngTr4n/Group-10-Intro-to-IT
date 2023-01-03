151d70cebef6c57b72c85ca5ca696eec897970cf
Hai Nguyen <s3978275@rmit.edu.vn>
2023-01-03 23:18:50 +0700

    navbar

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-// When the user scrolls the page, execute myFunction
+// sticky effect for header
 window.onscroll = function() {stickyEffect()};
-
-// Get the header
 var header = document.getElementById("stickyHeader");
-
-// Get the offset position of the navbar
 var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyEffect() 
 {
   if (window.pageYOffset > sticky) {
